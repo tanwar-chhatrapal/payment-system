ea517bd20902c88b4723705f5040f812bcf5e7b9
tanwar-chhatrapal <ctanwar@bestpeers.com>
2023-01-31 20:44:28 +0530

    added checkoutpage functionality

--- src/MockData/Products.js
+++ src/MockData/Products.js
@@ -2,17 +2,17 @@ export const AllProducts = [
     {
         PRODUCT_ID : "serram16",
         PRODUCT_NAME: "16GB RAM SERVER",
-        PRICE: "$1025.50"
+        PRICE: "1025.50"
     },
     {
         PRODUCT_ID : "serram32",
         PRODUCT_NAME: "32GB RAM SERVER",
-        PRICE: "$2549.99"
+        PRICE: "2549.99"
     },
     
     {
         PRODUCT_ID : "serram8",
         PRODUCT_NAME: "8GB RAM SERVER",
-        PRICE: "$100"
+        PRICE: "100"
     },
 ]
--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,80 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Checkout() {
+  const [cartProduct, setCartProduct] = useState();
+  const [coupanAmount, setCoupanAmount] = useState(0)
+  
+  useEffect(() => {
+    let cartproduct = JSON.parse(localStorage.getItem('product'))
+    setCartProduct(cartproduct);
+  }, [])
+
+  let totalAmount = cartProduct && cartProduct.reduce((previousValue, currentValue) => {
+      return Number(previousValue) + Number(currentValue.PRICE)
+  }, 0)
+
+  let coupanCode ='';
+  let total = totalAmount
+  if(totalAmount > 1000 && totalAmount < 10000) coupanCode = ('PLSD123')
+  if(totalAmount > 10000) coupanCode = 'PLSD456'
+
+  const handleCoupanCode = (e)  => {
+    e.preventDefault()
+    total = ((totalAmount * 10 ) / 100).toFixed(2);
+    setCoupanAmount(total)
+  }
+  
+  let finaAmount = (totalAmount - coupanAmount).toFixed(2)
   return (
-        <div class="row">
-          <div class="col-md-8">
-            <div class="table-responsive checkout">
-              <table class="table table-hover mt-5">
-                <thead class="table-dark border">
-                  <tr>
-                    <th scope="col" class="text-white">Product Id</th>
-                    <th class="text-white text-center">Product Name</th>
-                    <th class="text-white text-end">Price (per month)</th>
+    <div className="row">
+      <div className="col-md-8">
+        <div className="table-responsive checkout">
+          <table className="table table-hover mt-5">
+            <thead className="table-dark border">
+              <tr>
+                <th scope="col" className="text-white">Product Id</th>
+                <th className="text-white text-center">Product Name</th>
+                <th className="text-white text-end">Price (per month)</th>
+              </tr>
+            </thead>
+            <tbody className="border-top-0 border">
+              {cartProduct && cartProduct.map((product, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="fw-bold">{product.PRODUCT_ID}</td>
+                    <td align="center">{product.PRODUCT_NAME}</td>
+                    <td align="center">{product.PRICE}</td>
                   </tr>
-                </thead>
-                <tbody class="border-top-0 border">
-                  <tr>
-                    <td class="fw-bold">Serram16</td>
-                    <td align="center">16GB RAM SERVER</td>
-                    <td align="right"><span class="me-2">$1025.50</span> <small class="position-absolute"><i class="fa fa-times" aria-hidden="true"></i>
-                    </small></td>
-                  </tr>
-                  <tr>
-                    <td class="fw-bold">Serram32</td>
-                    <td align="center">32GB RAM SERVER</td>
-                    <td align="right"><span class="me-2">$2549.99</span> <small class="position-absolute"><i class="fa fa-times" aria-hidden="true"></i>
-                    </small></td>
-                  </tr>
-                  <tr>
-                    <td class="fw-bold">Serram8</td>
-                    <td align="center">8GB RAM SERVER</td>
-                    <td align="right"><span class="me-2">$100</span> <small class="position-absolute"><i class="fa fa-times" aria-hidden="true"></i>
-                    </small></td>
-                  </tr>
-                  <tr>
-                    <td colspan="2">
-                      <div class="input-group w-75">
-                        <input type="text" class="form-control border-dark" placeholder="Coupon code" aria-describedby="button-addon2"/>
-                          <button class="btn btn-secondary" type="button" id="button-addon2">Submit</button>
-                      </div>
-                    </td>
-                    <td class="fw-bold" align="right">$10</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="table-responsive checkout">
-              <table class="table table-hover mt-5">
-                <thead class="table-dark border">
-                  <tr>
-                    <th colspan="2" class="text-white"><h5>Cart Totals</h5></th>
-                  </tr>
-                </thead>
-                <tbody class="border">
-                  <tr>
-                    <td class="fw-bold">Discount</td>
-                    <td class="fw-bold" align="right">$100</td>
-                  </tr>
-                  <tr>
-                    <td class="fw-bold">Shipping</td>
-                    <td class="fw-bold" align="right">$100</td>
-                  </tr>
-                  <tr>
-                    <td class="fw-bold">Subtotal</td>
-                    <td class="fw-bold" align="right">$500</td>
-                  </tr>
-                  <tr class="table-dark border">
-                    <td class="fw-bold">Total</td>
-                    <td class="fw-bold" align="right">$700</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
+      </div>
+      <div className="col-md-4">
+        <div className="table-responsive checkout">
+          <table className="table table-hover mt-5">
+            <thead className="table-dark border">
+              <tr>
+                <th colspan="2" className="text-white"><h5>Cart Totals</h5></th>
+              </tr>
+            </thead>
+            <tbody className="border">
+              <tr>
+                <td className="fw-bold">Sub Total</td>
+                <td className="fw-bold" align="right">${totalAmount}</td>
+              </tr>
+              <tr>
+                <td colpan="2">
+                  <div className="input-group w-75">
+                  <input type="text" class="form-control border-dark" placeholder={coupanCode} aria-describedby="button-addon2"/>
+                    <button className="btn btn-secondary" type="button" id="button-addon2" onClick={handleCoupanCode}>Apply Promo</button>
+                  </div>
+                </td>
+                <td className="fw-bold" align="right">${coupanAmount}</td>
+              </tr>
+              <tr className="table-dark border">
+                <td className="fw-bold">Total</td>
+                <td className="fw-bold" align="right">${finaAmount}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
